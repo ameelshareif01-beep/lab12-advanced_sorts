8630fcdb6e96a7f01977a30dd2fa5242d3be23e3
ameelshareif01-beep <ameelshareif01@gmail.com>
2025-11-21 21:56:11 -0800

    Create AdvancedSortDriver.java

---
+++ AdvancedSortDriver.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+
+public class AdvancedSortDriver {
+    public static void main(String[] args) {
+
+        Comparator<Integer> comp = Comparator.naturalOrder();
+        int N = 20;  // You can increase to 100+ if desired
+        Random rand = new Random();
+
+        Integer[] arr1 = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            arr1[i] = rand.nextInt(100); 
+        }
+
+        Integer[] arr2 = Arrays.copyOf(arr1, arr1.length);
+
+        System.out.println("--- Test: Random Array (N=" + N + ") ---");
+        System.out.println("Original:  " + Arrays.toString(arr1));
+
+        AdvancedSorters.mergeSort(arr1, comp);
+        System.out.println("Merge Sort: " + Arrays.toString(arr1));
+
+        System.out.println("Original:  " + Arrays.toString(arr2));
+        AdvancedSorters.quickSort(arr2, comp);
+        System.out.println("Quick Sort: " + Arrays.toString(arr2));
+    }
+}
